56ab268d4d4248caa185b24ac5de953e9676acac
Domingo <domingorv62@gmail.com>
2024-11-11 00:05:47 -0300

    .

--- commands/Interacción/fakemessage.js
+++ commands/Interacción/fakemessage.js
@@ -3,6 +3,20 @@ import { createCanvas, GlobalFonts, loadImage } from '@napi-rs/canvas';
 import { parse } from 'twemoji-parser';
 import { request } from 'undici';
 
+export const data = new SlashCommandBuilder()
+.setName('fake-message')
+.setDescription('Genera una captura de un mensaje simulado.')
+.addUserOption(option =>
+  option.setName('usuario')
+  .setDescription('Elige un usuario')
+  .setRequired(true)
+)
+.addStringOption(option => 
+  option.setName('contenido')
+  .setDescription('Contenido del mensaje.')
+  .setRequired(true)
+);
+
 // Parámetros similares a la interfaz de Discord
 const parameters = {
   fontSize: 48,
@@ -33,49 +47,60 @@ Object.values(fonts).forEach((font) => {
 const canvasWidth = parameters.innerPadding * 4 + parameters.userPictureSize + parameters.userPictureRightMargin + parameters.messageContentWidth;
 const canvasBaseHeight = parameters.innerPadding * 2 + parameters.userPictureSize;
 
-export const data = new SlashCommandBuilder()
-.setName('fake-message')
-.setDescription('Genera una captura de un mensaje simulado.')
-.addUserOption(option =>
-  option.setName('usuario')
-  .setDescription('Elige un usuario')
-  .setRequired(true)
-)
-.addStringOption(option => 
-  option.setName('contenido')
-  .setDescription('Contenido del mensaje.')
-  .setRequired(true)
-);
+// Función para obtener el nombre de usuario correcto
+async function getDisplayName(interaction, user) {
+  try {
+    // Si está en un servidor
+    if (interaction.guild) {
+      const member = await interaction.guild.members.fetch(user.id);
+      if (member && member.displayName) {
+        return member.displayName;
+      }
+    }
+    
+    // Si no, obtener el nombre global
+    return user.globalName || user.username;
+  } catch (error) {
+    console.error('Ha ocurrido un error: ', error);
+    interaction.reply({ content: "<:Advertencia:1302055825053057084> Ha ocurrido un error al ejecutar el comando.", ephemeral: true, allowedMentions: { repliedUser: false }});
+  }
+}
 
 // Función para obtener el color del rol más alto del usuario
 function getUserColor(interaction, user) {
-  // Si es un mensaje directo, usar color blanco
-  if (!interaction.guild) {
+  try {
+    // Si es un mensaje directo, usar color blanco
+    if (!interaction.guild) {
+      return '#ffffff';
+    }
+    
+    // Obtener miembro del servidor
+    const member = interaction.guild.members.cache.get(user.id);
+    
+    // Si el usuario es miembro del servidor, usar el color del rol más alto del usuario
+    if (member && member.roles.highest && member.roles.highest.color) {
+      const colorHex = member.roles.highest.color.toString(16).padStart(6, '0');
+      return `#${colorHex}`;
+    }
+    
+    // Si no tiene roles o el rol no tiene color
     return '#ffffff';
+  } catch (error) {
+    console.error('Ha ocurrido un error: ', error);
+    interaction.reply({ content: "<:Advertencia:1302055825053057084> Ha ocurrido un error al ejecutar el comando.", ephemeral: true, allowedMentions: { repliedUser: false }});
   }
-  
-  // Obtener miembro del servidor
-  const member = interaction.guild.members.cache.get(user.id);
-  
-  // Si el usuario es miembro del servidor, usar el color del rol más alto del usuario
-  if (member && member.roles.highest && member.roles.highest.color) {
-    const colorHex = member.roles.highest.color.toString(16).padStart(6, '0');
-    return `#${colorHex}`;
-  }
-  
-  // Si no tiene roles o el rol no tiene color
-  return '#ffffff';
 }
 
-export async function run(client, interaction) {
-  // Obtener usuario y contenido del mensaje
-  const user = interaction.options.getUser('usuario');
-  const messageContent = interaction.options.getString('contenido');
-  
-  // Obtener color del rol del usuario
-  const userColor = getUserColor(interaction, user);
-  
+// Función run
+export async function run(client, interaction) {  
   try {
+    // Obtener usuario y contenido del mensaje
+    const user = interaction.options.getUser('usuario');
+    const messageContent = interaction.options.getString('contenido');
+    
+    // Obtener color del rol del usuario
+    const userColor = getUserColor(interaction, user);
+    
     // Crear canvas y contexto
     const canvas = createCanvas(canvasWidth, canvasBaseHeight);
     const context = canvas.getContext('2d');
@@ -90,9 +115,10 @@ export async function run(client, interaction) {
     context.textBaseline = 'top';
     
     // Dibujar nombre de usuario
+    const displayName = await getDisplayName(interaction, user);
     const userNamePosX = parameters.innerPadding + parameters.userPictureSize + parameters.userPictureRightMargin;
     const userNamePosY = parameters.innerPadding;
-    context.fillText(user.displayName, userNamePosX, userNamePosY);
+    context.fillText(displayName, userNamePosX, userNamePosY);
     
     // Dibujar el avatar
     const avatarPosX = parameters.innerPadding;
@@ -128,7 +154,7 @@ export async function run(client, interaction) {
     
     interaction.reply({ files: [{ attachment, name: 'fake-message.png' }], allowedMentions: { repliedUser: false } });
   } catch (error) {
-    console.error('Ha ocurrido un error:', error);
+    console.error('Ha ocurrido un error: ', error);
     interaction.reply({ content: "<:Advertencia:1302055825053057084> Ha ocurrido un error al ejecutar el comando.", ephemeral: true, allowedMentions: { repliedUser: false }});
   }
 }
