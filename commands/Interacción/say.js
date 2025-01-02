5697a41b4f7bde49aaf0fd1283c2e3548c2bd03c
Doming3000 <domingorv62@gmail.com>
2025-01-02 00:45:22 -0300

    .

--- commands/Interacción/say.js
+++ commands/Interacción/say.js
@@ -20,6 +20,9 @@ async function sendConfirmation(interaction, message = 'Mensaje enviado!') {
 export async function run(client, interaction) {
   const contenido = interaction.options.getString('contenido');
   
+  // Registrar en la consola al usuario que ejecutó el comando
+  console.log(`- ${interaction.user.tag} ${interaction.user.id} usó /say: ${contenido}.`);
+  
   // Comprobar si existe un canal de interacción; si no, se asume que es un mensaje directo
   if (!interaction.channel) {
     sendConfirmation(interaction);
@@ -27,6 +30,7 @@ export async function run(client, interaction) {
   } else {
     // Verificar si el contenido incluye menciones masivas
     const roleMentionPattern = /<@&\d+>/;
+    
     if (contenido.includes('@everyone') || contenido.includes('@here') || roleMentionPattern.test(contenido)) {
       await interaction.reply({ content: '<:Advertencia:1302055825053057084> ¿Estás tratando de hacer una mención masiva? Lo siento, no puedes hacer eso.', flags: 64 });
     } else {
