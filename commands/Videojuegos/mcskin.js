3bb428e1aebd8a8a75e4cd4eaf2ce386696f2a27
Doming3000 <domingorv62@gmail.com>
2025-01-02 22:33:03 -0300

    .

--- commands/Videojuegos/mcskin.js
+++ commands/Videojuegos/mcskin.js
@@ -1,69 +1,71 @@
 import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder } from "discord.js";
-import request from "request";
+import axios from "axios";
 
 export const data = new SlashCommandBuilder()
 .setName('mcskin')
 .setDescription('Muestra la skin de un jugador de Minecraft premium.')
-.addStringOption(option => 
+.addStringOption(option =>
   option.setName('nombre')
   .setDescription('Nick del jugador premium.')
   .setRequired(true)
 );
 
-export function run(client, interaction) {
+// Función principal que se ejecuta cuando el comando es llamado.
+export async function run(client, interaction) {
   const nombre = interaction.options.getString('nombre');
   const caracteres = nombre.length;
-
+  
+  // Validar si el nombre del jugador sobrepasa los 16 caracteres.
   if (caracteres > 16) {
-    interaction.reply({ content: "<:Advertencia:1302055825053057084> El nombre de este jugador sobrepasa los 16 caracteres.", flags: 64, allowedMentions: { repliedUser: false }});
+    interaction.reply({ content: "<:Advertencia:1302055825053057084> El nombre de este jugador sobrepasa los 16 caracteres.", flags: 64 , allowedMentions: { repliedUser: false }});
     return;
   }
   
-  let mojang_player_api = `https://api.mojang.com/users/profiles/minecraft/${nombre}`;
+  const mojangPlayerApi = `https://api.mojang.com/users/profiles/minecraft/${nombre}`;
   
-  request(mojang_player_api, function (err, resp, body) {
-    // Si hay un error con la solicitud, enviar un mensaje de error.
-    if (err) {
-      interaction.reply({ content: "<:Advertencia:1302055825053057084> Ocurrió un error al consultar la API.", flags: 64, allowedMentions: { repliedUser: false }});
-      return;
-    }
+  try {
+    // Indicar que estamos procesando la solicitud
+    await interaction.deferReply({ flags: 0 });
     
-    try {
-      body = JSON.parse(body);
-      
-      // Verificar si el jugador no existe
-      if (!body || !body.id) {
-        interaction.reply({ content: "<:Advertencia:1302055825053057084> No he podido encontrar al jugador.", flags: 64, allowedMentions: { repliedUser: false }});
-        return;
-      }
-      
-      let player_id = body.id;
-      let render = `https://mc-heads.net/body/${player_id}/128.png`;
-      let skin = `https://crafatar.com/skins/${player_id}.png`;
-      let avatar = `https://mc-heads.net/avatar/${player_id}.png`;
-      
-      let embed = new EmbedBuilder()
-      .setColor(0x94bf63)
-      .setAuthor({
-        name: `${client.user.username} - ${interaction.commandName}`,
-        iconURL: client.user.displayAvatarURL()
-      })
-      .setTitle(`Skin del jugador: ${body.name}`)
-      .setImage(render)
-      .setThumbnail(avatar);
-      
-      const actionRow = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-        .setEmoji("<:Imagen:1302064339829653535>")
-        .setLabel("Abrir en el navegador")
-        .setURL(`${skin}`)
-        .setStyle("Link")
-      );
-      
-      interaction.reply({ embeds: [embed], components: [actionRow], allowedMentions: { repliedUser: false } });
-    } catch (err) {
-      interaction.reply({ content: "<:Advertencia:1302055825053057084> Ha ocurrido un error al ejecutar el comando.", flags: 64, allowedMentions: { repliedUser: false }});
+    // Realizar una solicitud HTTP GET a la API usando axios.
+    const response = await axios.get(mojangPlayerApi);
+    
+    const body = response.data;
+    
+    // Extraer el ID del jugador y generar las URLs necesarias.
+    const playerId = body.id;
+    const render = `https://mc-heads.net/body/${playerId}/128.png`;
+    const skin = `https://crafatar.com/skins/${playerId}.png`;
+    const avatar = `https://mc-heads.net/avatar/${playerId}.png`;
+    
+    // Embed principal con la skin del jugador.
+    const embed = new EmbedBuilder()
+    .setColor(0x94bf63)
+    .setAuthor({
+      name: `${client.user.username} - ${interaction.commandName}`,
+      iconURL: client.user.displayAvatarURL()
+    })
+    .setTitle(`Skin del jugador: ${body.name}`)
+    .setImage(render)
+    .setThumbnail(avatar);
+    
+    // Botón para abrir la skin en el navegador.
+    const actionRow = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+      .setEmoji("<:Imagen:1302064339829653535>")
+      .setLabel("Abrir en el navegador")
+      .setURL(`${skin}`)
+      .setStyle("Link")
+    );
+    
+    // Enviar mensaje
+    interaction.editReply({ embeds: [embed], components: [actionRow], allowedMentions: { repliedUser: false }});
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      interaction.editReply({ content: "<:Advertencia:1302055825053057084> No se ha podido encontrar al jugador.", allowedMentions: { repliedUser: false }});
+    } else {
+      interaction.editReply({ content: "<:Advertencia:1302055825053057084> Ha ocurrido un error al ejecutar el comando.", allowedMentions: { repliedUser: false }});
     }
-  });
+  }
 }
