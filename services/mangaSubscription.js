032a86d57d19389260c9aa89da2ddc94a0c0e493
Doming3000 <domingorv62@gmail.com>
2025-05-27 23:54:40 -0400

    Added MangaScrapping service

--- services/mangaSubscription.js
+++ services/mangaSubscription.js
@@ -1,6 +1,33 @@
-// import { query } from '../database.js';
-// import { CronJob } from "cron";
+import { EmbedBuilder } from 'discord.js';
+import { query } from '../database.js';
+import { CronJob } from "cron";
+import axios from "axios";
 
-// hacer consultas cada hora por si el capitulo sube.
+// hacer consultas cada cierto tiempo por si el capitulo sube.
 // Si sube, enviar confirmación via MD al usuario suscrito
 // Si el manga finaliza, eliminar el registro y avisar.
+
+// Función para consultar la base de datos
+function checkNewChapters() {
+  try {
+    // Obtener el último capitulo registrado
+    query ("SELECT FROM mangasuscription WHERE ")
+    
+    
+  } catch {error} {
+    console.log(error);
+  }
+}
+
+function sendMessage() {
+  const embed = new EmbedBuilder()
+  .setColor(0x779ecb)
+  .setAuthor({
+    name: `${client.user.username} - ${interaction.commandName}`,
+    iconURL: client.user.displayAvatarURL()
+  })
+  .setTitle("Se ha subido un nuevo capítulo de un manga que estás leyendo")
+
+  
+  interaction.reply({ embeds: [embed], allowedMentions: { repliedUser: false } })
+}
