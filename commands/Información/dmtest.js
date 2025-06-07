2dd4a80dde9e1bfc906b05ef6c11841bb8e70086
Doming3000 <domingorv62@gmail.com>
2025-06-07 01:33:12 -0400

    Update dmtest.js

--- commands/Información/dmtest.js
+++ commands/Información/dmtest.js
@@ -3,14 +3,25 @@ import { SlashCommandBuilder } from "discord.js";
 export const data = new SlashCommandBuilder()
 .setName('dm-test')
 .setDescription('Comprueba la disponibilidad para recibir mensajes directos de la aplicación.')
+.addStringOption(option =>
+  option.setName('contenido')
+  .setDescription('Opcional. Contenido del mensaje.')
+  .setRequired(false)
+);
 
 export async function run(client, interaction) {
+  const contenido = interaction.options.getString('contenido');
+  
   try {
     // Gifs
     const gifs = ["https://tenor.com/9E01.gif", "https://tenor.com/bisuu.gif", "https://tenor.com/birIr.gif"];
     
     // Enviar mensaje directo al usuario
-    interaction.user.send({ content: gifs[Math.floor(Math.random() * gifs.length)], allowedMentions: { repliedUser: false }});
+    if(!contenido) {
+      await interaction.user.send({ content: gifs[Math.floor(Math.random() * gifs.length)], allowedMentions: { repliedUser: false }});
+    } else {
+      await interaction.user.send({ content: contenido, allowedMentions: { repliedUser: false }});
+    }
     
     // Confirmar la interacción
     const reply = await interaction.reply({ content: "<:Done:1326292171099345006> ¡Mensaje enviado!", flags: 64, allowedMentions: { repliedUser: false }});
