b9d45b52bc8ea1d16f7c49fcb7b6ebed65cbf19d
Doming3000 <domingorv62@gmail.com>
2025-06-15 01:17:49 -0400

    Updated botping.js

--- commands/Información/botping.js
+++ commands/Información/botping.js
@@ -6,25 +6,19 @@ export const data = new SlashCommandBuilder()
 .setDescription('Muestra la latencia y estado del bot.')
 
 export function run(client, interaction) {
+  // Obtener información del entorno
   const memoryUsage = process.memoryUsage();
   const totalMem = os.totalmem();
   const usedMem = memoryUsage.rss;
   const cpuLoad = os.loadavg()[0]; // Promedio de carga del CPU (último minuto)
-  const uptime = process.uptime(); // En segundos
   
-  const formatBytes = (bytes) => `${(bytes / 1024 / 1024).toFixed(2)} MB`;
-  const formatTime = (sec) => {
-    const h = Math.floor(sec / 3600);
-    const m = Math.floor((sec % 3600) / 60);
-    const s = Math.floor(sec % 60);
-    return `${h}h ${m}m ${s}s`;
-  };
+  const formatBytes = (bytes) => `${(bytes / 1024 / 1024).toFixed(2)}MB`;
   
   const embed = new EmbedBuilder()
   .setColor(0x779ecb)
   .setAuthor({ name: `${client.user.username} - ${interaction.commandName}`, iconURL: client.user.displayAvatarURL()})
   .setTitle('🏓 ¡Pong!')
-  .setDescription(`- 📡 Mi ping actual es \`${client.ws.ping}ms\`.\n- 🧠 Uso de memoria: \`${formatBytes(usedMem)} / ${formatBytes(totalMem)}\`\n- ⚙️ Carga del CPU (1min): \`${cpuLoad.toFixed(2)}\`\n- ⏱️ Uptime: \`${formatTime(uptime)}\``)
+  .setDescription(`- 📡 Ping:\`${client.ws.ping}ms\`.\n- 🧠 Uso de memoria: \`${formatBytes(usedMem)} / ${formatBytes(totalMem)}\`\n- 🤖 Carga del CPU (último minuto): \`${cpuLoad.toFixed(2)}\``)
   
   interaction.reply({ embeds: [embed], allowedMentions: { repliedUser: false } })
 }
