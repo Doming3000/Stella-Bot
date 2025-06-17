4a9d2d0f9d21f600907f4a02655ceebf24c37a30
Doming3000 <domingorv62@gmail.com>
2025-06-17 19:28:55 -0400

    Updated botInfo.js

--- commands/Información/botInfo.js
+++ commands/Información/botInfo.js
@@ -26,54 +26,54 @@ function countCommandFiles(dir) {
   return fileCount;
 }
 
-// Función para determinar el entorno
-function getEnvironment() {
-  const hostname = os.hostname();
-  const localHostname = 'LAPTOP-BNS69TVB';
-  
-  if (hostname === localHostname) {
-    return 'Local';
-  } else {
-    return 'Alojado';
-  }
-}
-
 // Contar la cantidad de comandos dinámicamente
 const commandCount = countCommandFiles('./commands');
 
 // Obtener la versión de Node.js
 const nodeVersion = process.version;
 
-async function getOwnerMention(interaction) {
-  const ownerId = '811071747189112852';
+// Función para obtener al propietario
+async function getOwner(interaction) {
+  const ownerName = '**Doming3000**';
   
   // Comprobar si existe un canal de interacción; Si no, se asume que es un mensaje directo
   if (!interaction.guild) {
-    return "**Doming3000**";
+    return ownerName;
   }
   
   try {
-    // Intentar encontrar al usuario en el servidor
-    const member = await interaction.guild.members.fetch(ownerId);
+    // Intentar encontrar al usuario en el servidor mediante su ID
+    const member = await interaction.guild.members.fetch('811071747189112852');
     return `<@${member.id}>`;
   } catch (error) {
-    return "**Doming3000**";
+    return ownerName;
+  }
+}
+
+// Función para determinar el entorno
+function getEnvironment() {
+  const hostname = os.hostname();
+  
+  if (hostname === "9da2cfa3-0a58-4c41-88e8-b11539556fa2") { // Vigilar que este sea el nombre correcto
+    return '<:Nube:1324564802969931818> **Hosteado**'
+  } else {
+    return '<:Settings:1384039104421036154> **Local**'
   }
 }
 
 export async function run(client, interaction) {
   const uptime = Math.floor((new Date().getTime() - client.uptime) / 1000);
-  const ownerMention = await getOwnerMention(interaction);
+  const ownerMention = await getOwner(interaction);
   
   const embed = new EmbedBuilder()
   .setColor(0x779ecb)
   .setAuthor({ name: `${client.user.username} - ${interaction.commandName}`, iconURL: client.user.displayAvatarURL()})
   .setTitle("🔎 Información de la aplicación:")
-  .setDescription(`**¡Hola!**\nSoy una aplicación privada diseñada para servidores de confianza.\nFui puesta en línea <t:${uptime}:R> y cuento con **${commandCount}** comandos.`)
+  .setDescription(`**¡Hola!**\nSoy una aplicación privada diseñada para servidores de confianza.\nLlevo en funcionamiento desde <t:${uptime}:R> y cuento con **${commandCount}** comandos.`)
   .addFields(
     { name: "📚 - Versiones", value: `➜ <:Discord:1302057111550824561> **Discord.js**: \`${discordVersion}\`\n➜ <:Nodejs:1302056618971889757> **Node.js**: \`${nodeVersion}\``, inline: true },
     { name: "📃 - Información Adicional:", value: `➜ <:Javascript:1302106857724448872> Programado con **Javascript**\n➜ :technologist: Desarollado por: ${ownerMention}`, inline: true },
-    { name: "🖥️ - Entorno actual", value: `➜ <:Nube:1324564802969931818> **${getEnvironment()}**`, inline: false }
+    { name: "🖥️ - Entorno Actual", value: `➜ ${getEnvironment()}`, inline: false }
   );
   
   await interaction.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
